7625ee81fb15b9414284bcabfa1b31cf0f938df2
micahmosley <micah.mosley@utexas.edu>
2020-07-27 17:05:00 -0500

    changed :true_or_false from boolean to string

--- db/migrate/001_create_facts.rb
+++ db/migrate/001_create_facts.rb
@@ -2,7 +2,7 @@ class CreateFacts < ActiveRecord::Migration[5.0]
     def change
       create_table :facts do |t|
         t.string :fact
-        t.boolean :true_or_false
+        t.string :true_or_false
       end
     end
   end
