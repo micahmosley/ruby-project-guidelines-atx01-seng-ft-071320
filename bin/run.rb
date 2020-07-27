74ae9ddd03878ec0c44150ac348ff48361ade257
reedanna <reedanna@mail.com>
2020-07-27 15:53:50 -0500

    run.rb now has text scroll down the screen

--- bin/run.rb
+++ bin/run.rb
@@ -1,13 +1,31 @@
 require_relative '../config/environment'
+def print_text(lines_top, lines_bottom, text)
+    lines_top.times do
+        print "\n"
+    end
+    print text
+    lines_bottom.times do
+        print "\n"
+    end
+end
 
-game1 = Game.create(username: "Anna", score: 50)
-game2 = Game.create(username: "Dagon", score: 20)
-game3 = Game.create(username: "Kerry", score: 25)
-game4 = Game.create(username: "Shannon", score: 30)
-game5 = Game.create(username: "Cricket", score: 15)
-game6 = Game.create(username: "Max", score: 23)
-game7 = Game.create(username: "Jonny", score: 22)
-game8 = Game.create(username: "Eleanor", score: 40)
-game9 = Game.create(username: "Brigid", score: 33)
-game10 = Game.create(username: "Pogo", score: 12)
-Game.high_scores
+def time_passing
+    time = Time.now.to_i
+    until Time.now.to_i > time 
+    end
+end
+
+lines = 4
+text = "hello world"
+bottomlines = lines
+
+while bottomlines >= 0
+    #a second passes
+    time_passing
+    #this clears the terminal of all existing text
+    puts "\e[2J\e[f"
+    print_text(lines - bottomlines, bottomlines, text)
+    bottomlines -= 1
+end
+
+
