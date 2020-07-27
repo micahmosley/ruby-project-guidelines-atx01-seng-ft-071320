37ecfaa4094bb8379caec774003ad95bfac58a65
micahmosley <micah.mosley@utexas.edu>
2020-07-27 16:51:04 -0500

    fixed key from text: to fact:

--- lib/import_to_database.rb
+++ lib/import_to_database.rb
@@ -4,6 +4,6 @@ class ImportToDatabase
     response = Net::HTTP.get_response(uri)
     fact_hash=JSON.parse(response.body)
     fact_hash['results'].each do |fact| 
-        Fact.create(text: fact["question"], true_or_false: fact["correct_answer"])
+        Fact.create(fact: fact["question"], true_or_false: fact["correct_answer"])
     end 
 end 
