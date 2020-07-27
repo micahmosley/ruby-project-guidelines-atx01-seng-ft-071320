102a1752dd314c8670bb2b8ce0f91bbf79930652
reedanna <reedanna@mail.com>
2020-07-27 14:41:49 -0500

    added high_scores method to game.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,13 @@
 require_relative '../config/environment'
 
-print "hello world"
+game1 = Game.create(username: "Anna", score: 50)
+game2 = Game.create(username: "Dagon", score: 20)
+game3 = Game.create(username: "Kerry", score: 25)
+game4 = Game.create(username: "Shannon", score: 30)
+game5 = Game.create(username: "Cricket", score: 15)
+game6 = Game.create(username: "Max", score: 23)
+game7 = Game.create(username: "Jonny", score: 22)
+game8 = Game.create(username: "Eleanor", score: 40)
+game9 = Game.create(username: "Brigid", score: 33)
+game10 = Game.create(username: "Pogo", score: 12)
+Game.high_scores
