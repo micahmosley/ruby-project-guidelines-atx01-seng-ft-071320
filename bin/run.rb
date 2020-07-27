c0235e10782ee376b57330615436a610dd1a386d
reedanna <reedanna@mail.com>
2020-07-27 17:31:52 -0500

    framework for answering questions finished?

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,8 @@
 require_relative '../config/environment'
 
+#method that prints test in the correct vertical position
 def print_text(lines_top, lines_bottom, text)
+    puts "\e[2J\e[f"
     lines_top.times do
         print "\n"
     end
@@ -10,24 +12,36 @@ def print_text(lines_top, lines_bottom, text)
     end
 end
 
+#method that just lets a second pass before moving on
 def time_passing
     time = Time.now.to_i
     until Time.now.to_i > time 
     end
 end
 
-lines = 4
+
+lines = 10
 text = "hello world"
 bottomlines = lines
 
+scrolling_text = Thread.new{
 while bottomlines >= 0
-    #a second passes
     time_passing
-    #this clears the terminal of all existing text
-    puts "\e[2J\e[f"
     print_text(lines - bottomlines, bottomlines, text)
     bottomlines -= 1
 end
+print_text(5, 5, "you lose!")
+}
+
+answer = gets.chomp
+if answer == "t"
+    scrolling_text.kill
+    print_text(5, 5, "you win!")
+else
+    scrolling_text.kill
+    print_text(5, 5, "you lose!")
+end
+scrolling_text.join
 
 
 
