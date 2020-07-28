a04f40c2c1820c2f2ec312c9b74971ea4152f2a2
reedanna <reedanna@mail.com>
2020-07-28 10:41:20 -0500

    let questions appear at a random horizontal position

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,13 @@
 require_relative '../config/environment'
 
 #method that prints test in the correct vertical position
-def print_text(lines_top, lines_bottom, text)
+def print_text(lines_top, lines_bottom, spaces, text)
     puts "\e[2J\e[f"
     lines_top.times do
         print "\n"
+    end
+    spaces.times do
+        print " "
     end
     print text
     lines_bottom.times do
@@ -12,36 +15,32 @@ def print_text(lines_top, lines_bottom, text)
     end
 end
 
-#method that just lets a second pass before moving on
-def time_passing
-    time = Time.now.to_i
-    until Time.now.to_i > time 
+#method that sends a fact falling from the top of the screen and takes in an answer
+def ask_question(lines, text)
+    bottomlines = lines
+
+    scrolling_text = Thread.new{
+        spaces = rand(150)
+        while bottomlines >= 0
+            sleep(1)
+            print_text(lines - bottomlines, bottomlines, spaces, text)
+            bottomlines -= 1
+        end
+        print_text(5, 5, 10, "you lose!")
+    }
+
+    answer = gets.chomp
+    if answer == "t"
+        scrolling_text.kill
+        print_text(5, 5, 10, "you win!")
+    else
+        scrolling_text.kill
+        print_text(5, 5, 10, "you lose!")
     end
+    scrolling_text.join
 end
 
-
-lines = 10
-text = "hello world"
-bottomlines = lines
-
-scrolling_text = Thread.new{
-while bottomlines >= 0
-    time_passing
-    print_text(lines - bottomlines, bottomlines, text)
-    bottomlines -= 1
-end
-print_text(5, 5, "you lose!")
-}
-
-answer = gets.chomp
-if answer == "t"
-    scrolling_text.kill
-    print_text(5, 5, "you win!")
-else
-    scrolling_text.kill
-    print_text(5, 5, "you lose!")
-end
-scrolling_text.join
+ask_question(10, "hello world")
 
 
 
