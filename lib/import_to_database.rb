b8ff4a41b067b273e9ee0da24b7736850b8ce963
micahmosley <micah.mosley@utexas.edu>
2020-07-29 11:35:21 -0500

    added functionality to fix formatting of questions

--- lib/import_to_database.rb
+++ lib/import_to_database.rb
@@ -6,4 +6,27 @@ class ImportToDatabase
     fact_hash['results'].each do |fact| 
         Fact.create(fact: fact["question"], true_or_false: fact["correct_answer"])
     end 
+
+    #Fix formatting of our questions.
+    #Replaces '&quot;' with " " and replaces '&#039;' with an '
+    Fact.all.each do |fact|
+        i=fact.fact.sub! '&quot;', "'" 
+        j=fact.fact.sub! '&#039;', "'" 
+        if i && j==nil
+            while i do
+                i=fact.fact.sub! '&quot;', "'" 
+            end 
+        elsif j && i==nil
+            while j do 
+                j=fact.fact.sub! '&#039;', "'" 
+            end 
+        elsif i && j 
+            while i||j do 
+                i=fact.fact.sub! '&quot;', "'" 
+                j=fact.fact.sub! '&#039;', "'" 
+            end 
+        end 
+
+    end
+
 end 
