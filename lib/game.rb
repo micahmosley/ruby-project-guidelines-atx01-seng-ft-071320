2bb55fb0fc58541df9ce88567a8801bffc7fe5fb
micahmosley <micah.mosley@utexas.edu>
2020-07-29 10:03:17 -0500

    added functionality to Game.high_scores

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,10 @@ class Game < ActiveRecord::Base
     has_many :facts, through: :questions
 
     def self.high_scores
-        Game.all.order(score: :desc).limit(10)
+        games=Game.all.order(score: :desc).limit(5)
+        games.each do |game|
+            print game.username+"   "
+            puts game.score
+        end 
     end
 end 
