8be7d75e780be221f1a2a8b11fd1374c696ef092
reedanna <reedanna@mail.com>
2020-07-29 14:43:59 -0500

    retrying after game over now takes the pplayer directly to the new game

--- bin/run.rb
+++ bin/run.rb
@@ -2,3 +2,18 @@ require_relative '../config/environment'
 
 game = Controller.new
 game.intro
+
+def retry?
+    print_text(5, 5, 75, "Retry? (Y/N)")
+ 
+    y_or_n=gets.chomp 
+    if y_or_n.downcase=="y"
+        @@last_game = @@current_game
+        @@current_game = Game.new(username: @@last_game.username)
+        begin_game
+    elsif y_or_n.downcase=="n"
+        return
+    else
+        retry?
+    end
+end
