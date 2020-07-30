affacc97f46f712e0d2df60dca810d1bf2ca57fa
reedanna <reedanna@mail.com>
2020-07-30 10:37:07 -0500

    Added Game.high_score_table method

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,24 @@ class Game < ActiveRecord::Base
     has_many :facts, through: :questions
 
     def self.high_scores
-        games=Game.all.order(score: :desc).limit(5)
-        games.each do |game|
-            print game.username+"   "
-            puts game.score
-        end 
+        Game.all.order(score: :desc).limit(5)
+        # games.each do |game|
+        #     print game.username+"      "
+        #     puts game.score.to_s.yellow
+        # end 
+    end
+
+    def self.high_score_table
+        table = Terminal::Table.new do |t|
+            t.style = {:width => 50}
+            t.headings = [' ', 'Player', 'Score']
+            i = 0
+            while i < Game.high_scores.length do
+                t.add_separator
+                t.add_row [i+1, Game.high_scores[i].username, Game.high_scores[i].score]
+                i += 1
+            end
+        end
+        print table
     end
 end 
